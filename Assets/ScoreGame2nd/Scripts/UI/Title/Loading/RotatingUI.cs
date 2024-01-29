2f1d0c0175de5d639f7777d2014556bdadca4918
MDaiking <135773528+MDaiking@users.noreply.github.com>
2024-01-29 09:14:24 +0900

    metya yabai

---
+++ Assets/ScoreGame2nd/Scripts/UI/Title/Loading/RotatingUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class RotatingUI : MonoBehaviour
+{
+	private bool isRotating;
+	[SerializeField]
+	private float rotateSpeed;
+	private RectTransform rTransform;
+	public bool IsRotating
+	{
+		get { return isRotating; }
+		set { isRotating = value; }
+	}
+	private void Start()
+	{
+		rTransform = GetComponent<RectTransform>();
+	}
+	private void Update()
+	{
+		if (isRotating)
+		{
+			Quaternion rotate = Quaternion.AngleAxis(rotateSpeed, Vector3.forward);
+			rTransform.rotation *= rotate;
+		}
+	}
+}
